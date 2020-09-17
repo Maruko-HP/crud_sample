b632fd3211990323b5f29123386a5a5dfe90934b
Maruko-HP <hp_maru@icloud.com>
2020-09-18 00:10:16 +0900

    fix crud_resources_sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index"
-  get "/users/new", to: "users#new"
-  post "/users", to: "users#create"
-  get "/users/:id/edit", to: "users#edit"
+  resources :users
+  # get "/users", to: "users#index"
+  # get "/users/new", to: "users#new"
+  # post "/users", to: "users#create"
+  # get "/users/:id/edit", to: "users#edit"
 
-  # 以下のルーティングを追加
-  patch "/users/:id", to: "users#update"
+  # # 以下のルーティングを追加
+  # patch "/users/:id", to: "users#update"
 
-  delete "/users/:id", to: "users#destroy"
+  # delete "/users/:id", to: "users#destroy"
 end
